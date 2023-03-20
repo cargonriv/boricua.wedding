e0d258645f52718bb3568c60837acf89db89631a
cargonriv <cargonriv@pm.me>
2023-03-20 03:37:39 -0500

    added gallery, RSVP, and location subpages

--- js/script.js
+++ js/script.js
@@ -1,51 +1,122 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll(".slide");
+const images = document.querySelectorAll(".image-wrapper");
+let currentIndex = 0;
+let timer;
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.opacity = i === index ? 1 : 0;
-    slide.classList.toggle("active", i === index);
+function startSlider() {
+  timer = setInterval(() => {
+    currentIndex++;
+    if (currentIndex > images.length - 1) {
+      currentIndex = 0;
+    }
+    setActiveImage();
+  }, 3000);
+}
+
+function setActiveImage() {
+  images.forEach((image, index) => {
+    if (index === currentIndex) {
+      image.classList.add("active");
+      image.classList.remove("inactive");
+      const img = image.querySelector("img");
+      const imageWidth = img.naturalWidth;
+      const imageHeight = img.naturalHeight;
+      const ratio = imageWidth / imageHeight;
+      if (ratio > 1) {
+        img.classList.add("landscape");
+        img.classList.remove("portrait");
+      } else {
+        img.classList.add("portrait");
+        img.classList.remove("landscape");
+      }
+    } else {
+      image.classList.remove("active");
+      image.classList.add("inactive");
+      image.querySelector("img").style.transform = "translate(-50%, -50%)";
+    }
   });
 }
 
-function nextSlide() {
-  slideIndex = (slideIndex + 1) % slides.length;
-  showSlide(slideIndex);
+function resetTimer() {
+  clearInterval(timer);
+  startSlider();
 }
 
-function previousSlide() {
-  slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-  showSlide(slideIndex);
+function init() {
+  setActiveImage();
+  startSlider();
 }
 
-function autoChangeSlide() {
-  nextSlide();
-  setTimeout(autoChangeSlide, 3000);
-}
+init();
 
-function checkImageOrientation(img) {
-  const slide = img.closest(".slide");
-  if (img.naturalWidth > img.naturalHeight) {
-    console.log("Horizontal!!!");
-    img.classList.add("horizontal");
-  } else {
-    console.log("Vertical!!!");
-    slide.classList.add("vertical");
-    img.classList.add("vertical");
+// Gallery Modal
+
+const galleryImages = document.querySelectorAll(".gallery-img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modal-img");
+const modalClose = document.querySelector(".modal-close");
+
+galleryImages.forEach((img) => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    document.body.style.overflow = "hidden";
+  });
+});
+
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "auto";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
   }
-}
+});
 
-function initImageSlider() {
-  const images = document.querySelectorAll(".slide img");
-  images.forEach((img) => {
-    img.onload = () => {
-      checkImageOrientation(img);
-    };
-  });
-}
+// RSVP Form Validation
 
-document.addEventListener("DOMContentLoaded", () => {
-  initImageSlider();
-  showSlide(slideIndex);
-  setTimeout(autoChangeSlide, 3000);
+const form = document.querySelector("#rsvp-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const attendingInput = document.querySelector("#attending");
+const allergiesInput = document.querySelector("#allergies");
+const submitButton = document.querySelector("#submit-button");
+const formMessage = document.querySelector("#form-message");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nameValue = nameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const attendingValue = attendingInput.checked ? "Yes" : "No";
+  const allergiesValue = allergiesInput.value.trim();
+
+  if (
+    nameValue === "" ||
+    emailValue === "" ||
+    attendingValue === "" ||
+    allergiesValue === ""
+  ) {
+    formMessage.textContent = "Please fill out all fields.";
+    return;
+  }
+
+  formMessage.textContent = "Submitting...";
+  submitButton.disabled = true;
+
+  // Make AJAX request to submit the form data
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "submit-rsvp.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+      formMessage.textContent = xhr.responseText;
+      submitButton.disabled = false;
+    }
+  };
+  xhr.send(
+    `name=${nameValue}&email=${emailValue}&attending=${attendingValue}&allergies=${allergiesValue}`
+  );
 });
